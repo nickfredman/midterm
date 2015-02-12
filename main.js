06be4b14b710cbeadb84825add4cc36baa9068a8
Nick Fredmangit config --global ntfredman@gmail.comgit config --global user.name Nick <ntfredman@gmail.com>
2015-02-11 17:45:05 -0700

    Modal completed

--- main.js
+++ main.js
@@ -1,7 +1,21 @@
 var langs = ["javascript", "jquery"];
-
+var diff, currentLang;
 
 
 $(document).on('ready', function() {
+    $(document).on('click', '.lang-box', function() {
+        $('#js-modal').modal('show');
+        currentLang = $(this).val();
+        console.log(currentLang);
+    });
+    $(document).on('click', '.easy-mode', function() {
+        diff = 'easy';
 
+    });
+    $(document).on('click', '.medium-mode', function() {
+        diff = 'medium';
+    });
+    $(document).on('click', '.hard-mode', function() {
+        diff = 'hard';
+    });
 });
